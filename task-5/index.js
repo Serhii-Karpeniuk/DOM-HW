5f06d7caec44e86fbc12f130e92077f0c1473f54
Serhii-Karpeniuk <142886841+Serhii-Karpeniuk@users.noreply.github.com>
2023-11-03 12:54:41 +0200

    Update index.js

--- task-5/index.js
+++ task-5/index.js
@@ -2,8 +2,7 @@ const circleClass = document.querySelectorAll('.circle');
 
 circleClass.forEach((element) => {
     const attributeValue = element.getAttribute('data-anim');
-
-    console.log(attributeValue);
+    
     element.classList.add(attributeValue);
     
 
